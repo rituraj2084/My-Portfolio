5141d88f183f58f71bfd66c27559e112a79b2312
RItu Raj <rajritu2084@gmail.com>
2024-01-21 22:27:22 +0530

    Updated twitter link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,57 +1,54 @@
 import React from "react";
-import {Container, Row, Col} from "react-bootstrap";
-import {AiFillGithub, AiOutlineTwitter} from "react-icons/ai";
+import { Container, Row, Col } from "react-bootstrap";
+import { AiFillGithub, AiOutlineTwitter } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
 
 function Footer() {
-    let date = new Date();
-    let year = date.getFullYear();
-    return (
-      <Container fluid className="footer">
-        <Row>
-  
-          <Col md="6" className="footer-copywright">
-            <h3>Copyright © {year} Ritu Raj</h3>
-          </Col>
-          <Col md="6" className="footer-body">
-            <ul className="footer-icons">
-              <li className="social-icons">
-                <a
-                  href="https://github.com/rituraj2084"
-                  style={{ color: "white" }}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <AiFillGithub />
-                </a>
-              </li>
-              <li className="social-icons">
-                <a
-                  href="https://twitter.com/_raj_ritu"
-                  style={{ color: "white" }}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <AiOutlineTwitter />
-                </a>
-              </li>
-              <li className="social-icons">
-                <a
-                  href="https://www.linkedin.com/in/rituraj2084/"
-                  style={{ color: "white" }}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <FaLinkedinIn />
-                </a>
-              </li>
-  
-            </ul>
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
-  
-  export default Footer;
-  
+  let date = new Date();
+  let year = date.getFullYear();
+  return (
+    <Container fluid className="footer">
+      <Row>
+        <Col md="6" className="footer-copywright">
+          <h3>Copyright © {year} Ritu Raj</h3>
+        </Col>
+        <Col md="6" className="footer-body">
+          <ul className="footer-icons">
+            <li className="social-icons">
+              <a
+                href="https://github.com/rituraj2084"
+                style={{ color: "white" }}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <AiFillGithub />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="https://twitter.com/rituraj2084"
+                style={{ color: "white" }}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <AiOutlineTwitter />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="https://www.linkedin.com/in/rituraj2084/"
+                style={{ color: "white" }}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaLinkedinIn />
+              </a>
+            </li>
+          </ul>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default Footer;
